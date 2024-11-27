b6ff21abb58ae6ff31a3f23ee1344c6682e2185a
Ahmed Hany <ah7608867@gmail.com>
2024-11-27 06:45:10 +0200

    feat[server]: init auth service

--- src/Models/Auth/service/auth-service.ts
+++ src/Models/Auth/service/auth-service.ts
@@ -1,1 +1,24 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
+
+import { appError, AuthError} from '../../../Common/error/appError';
+
+interface authRequest extends Request {
+	profile: {
+		role: string
+	}
+}
+
+class AuthService{
+
+	public restrictTo(...roles: string[]) {
+
+		return async (request: authRequest, response: Response, next: NextFunction) => {
+			if (!roles.includes(request.profile.role)) return next(new AuthError('You do not have permission to perform this action', 403));
+			next();
+		}
+
+	}
+}
+
+const authService = new AuthService();
+export default authService;
