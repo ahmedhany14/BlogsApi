ead0b22ebdfe0930e327823855e2cfc951583e79
Ahmed Hany <ah7608867@gmail.com>
2024-11-27 04:41:54 +0200

    init server

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express"
+
+class appRouter {
+    private router: express.Router
+
+    constructor() {
+        this.router = express.Router()
+    }
+
+    public getInitialRoute() {
+        return this.router;
+    }
+}
+
+const router = new appRouter().getInitialRoute();
+export const app = express();
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import router, {app} from "./app";
+
+import express, {Request, Response, NextFunction} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.config.env'
+});
+
+app.use('/', (req: Request, res:Response, next: NextFunction): void => {
+    res.send('welcome to the server');
+});
+
+app.use('/blogApi', router);
+
+app.use('*', (req: Request, res: Response, next: NextFunction): void => {
+    res.status(404).send('Page not found');
+});
+
+app.listen(process.env.PORT, (): void => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
